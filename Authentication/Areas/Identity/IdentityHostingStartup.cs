4a846e150f40d5158cd74548bdc619b33e16ed06
Durant van Rensburg <durantvr18@gmail.com>
2020-10-01 17:40:34 -0400

    Add project files.

---
+++ Authentication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Authentication.Areas.Identity.Data;
+using Authentication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Authentication.Areas.Identity.IdentityHostingStartup))]
+namespace Authentication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                        options.SignIn.RequireConfirmedAccount = false            
+                    ).AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
